7b23ec650862a13add36c017fc3e47942fc42db4
zwei <Deus2077@proton.me>
2025-09-03 22:28:03 +1000

    installer
    please fucking work

---
+++ install.sh
@@ -0,0 +1,19 @@
+sudo pacman -S hyprland waybar git cava fastfetch flameshot hyprlock wofi yazi neovim kitty dunst
+git clone https://github.com/ZW31H4NDR/dotfiles
+cd dotfiles
+cp -r ~dotfiles/Dotfiles/cava ~/.config
+cp -r ~dotfiles/Dotfiles/dunst ~/.config
+cp -r ~dotfiles/Dotfiles/fastfetch ~/.config
+cp -r ~dotfiles/Dotfiles/flameshot ~/.config
+cp -r ~dotfiles/Dotfiles/hypr ~/.config
+cp -r ~dotfiles/Dotfiles/kitty ~/.config
+cp -r ~dotfiles/Dotfiles/nvim ~/.config
+cp -r ~dotfiles/Dotfiles/waybar ~/.config
+cp -r ~dotfiles/Dotfiles/wofi ~/.config
+cp -r ~dotfiles/Dotfiles/yazi ~/.config
+cp -r ~dotfiles/Backgrounds/Moon1.jpg ~/Pictures
+cp -r ~dotfiles/Backgrounds/Moon2.jpg ~/Pictures
+cp -r ~dotfiles/Backgrounds/Moon3.jpg ~/Pictures
+cp -r ~dotfiles/Backgrounds/Moon4.jpg ~/Pictures
+cp -r ~dotfiles/Backgrounds/Moon4.jpg ~/Pictures
+sudo reboot
